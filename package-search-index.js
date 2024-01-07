c267fb457067cf0b6f9b5c4ac0091d642c0758ac
ColdStone37 <83451149+ColdStone37@users.noreply.github.com>
2024-01-07 17:27:35 +0000

    Deploying to javadoc from @ ColdStone37/YearReview@204eb0abd4e607942928fc07d99978306c48da7d 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"yearreview.app"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"yearreview.app"},{"l":"yearreview.app.config"},{"l":"yearreview.app.render"}];updateSearchResults();
--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"yearreview.app","l":"App"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"yearreview.app","l":"App"},{"p":"yearreview.app.config","l":"GlobalSettings"},{"p":"yearreview.app.render","l":"Renderer"},{"p":"yearreview.app.render","l":"VideoWorker"}];updateSearchResults();
