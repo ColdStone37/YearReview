41188e46d0ecae72ca194fe47c059716e7dd9867
ColdStone37 <83451149+ColdStone37@users.noreply.github.com>
2024-02-23 00:53:43 +0100

    Committed changes in Main so CI passes

--- src/main/java/yearreview/app/App.java
+++ src/main/java/yearreview/app/App.java
@@ -1,19 +1,9 @@
 package yearreview.app;
 
 import yearreview.app.data.DataManager;
-import yearreview.app.data.processor.toplist.TopListElement;
-import yearreview.app.data.processor.toplist.TopListGenerator;
-import yearreview.app.data.sources.audio.AudioDataSource;
-import yearreview.app.data.sources.audio.database.AudioData;
-import yearreview.app.data.sources.audio.database.AudioTopListAdapter;
 import yearreview.app.grid.GridManager;
 import yearreview.app.render.Renderer;
 import yearreview.app.config.ConfigParser;
-import yearreview.app.util.value.DurationValue;
-
-import java.time.Duration;
-import java.time.Instant;
-import java.util.List;
 
 /**
  * Main Application
@@ -27,12 +17,7 @@ public class App {
 		GridManager grid = new GridManager(c.getWidgetSettings());
 		DataManager data = new DataManager(c.getDataSourcesSettings());
 		data.loadData();
-		TopListGenerator listGen = new TopListGenerator(((AudioDataSource) data.getSourceByTag("audio")).getTopListAdapter(AudioData.Type.Song), 50);
-		List<TopListElement> l = listGen.getTopList(Instant.now());
-		for(TopListElement elem:l){
-			System.out.println(elem.getValue().toString() + " - " + elem.getItem().getMainText());
-		}
-		//Renderer r = new Renderer(grid);
-		//r.renderVideo();
+		Renderer r = new Renderer(grid);
+		r.renderVideo();
 	}
 }
